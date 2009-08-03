01ddeb0dfb8d517eaef9a248bbc710b655a90bc5
Robin Burchell <w00t@inspircd.org>
2009-08-03 22:56:46 +0100

    Add modules.lst and script to generate them.

---
+++ regen-modules.php
@@ -0,0 +1,58 @@
+<?php
+
+function get_tags($sFile)
+{
+	$mfc = file_get_contents($sFile);
+    preg_match_all('@/\*\s*\$([^ ]+)\:\s*(.+?)\s*\*/@i', $mfc, $matches, PREG_SET_ORDER);
+
+
+	foreach ($matches as $amatch)
+	{
+		$lolmatch[$amatch[1]][] = $amatch[2];
+	}
+
+	return $lolmatch;
+}
+
+
+	$sText = "";
+
+	foreach (glob("m_*.cpp") as $sFile)
+	{
+		$aOut = array();
+		exec("git log -n 1 " . $sFile, $aOut);
+		$aOut = explode(" ", $aOut[0]);
+
+		$aTags = get_tags($sFile);
+		
+
+		$sStrippedName = str_replace(".cpp", "", $sFile);
+		$sText .= "module " . $sStrippedName . " " . $aOut[1] . " http://www.inspircd.com/modules/" . $sFile . "\n";
+
+		if ($aTags['ModDepends'])
+		{
+			foreach ($aTags['ModDepends'] as $sDepend)
+				$sText .= " depends " . $sDepend . "\n";
+		}
+
+		if ($aTags['ModConflicts'])
+		{
+			foreach ($aTags['ModConflicts'] as $sConflicts)
+				$sText .= " conflicts " . $sConflicts . "\n";
+		}
+
+		if ($aTags['ModDesc'])
+		{
+			foreach ($aTags['ModDesc'] as $sDesc)
+				$sText .= " description " . $sDesc . "\n";
+		}
+
+		if ($aTags['ModMask'])
+		{
+			foreach ($aTags['ModMask'] as $sMask)
+				$sText .= " mask " . $sMask . "\n";
+		}
+	}
+
+	file_put_contents("modules.lst", $sText);
+?>
