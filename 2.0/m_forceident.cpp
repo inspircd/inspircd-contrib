c91fea1be6fe0178ae8d6c3cb60860dfa57f1516
Peter Powell <petpow@saberuk.com>
2013-12-17 01:00:14 +0000

    Add a module for forcing idents on users based on their connect class.

---
+++ 2.0/m_forceident.cpp
@@ -0,0 +1,54 @@
+/*
+ * InspIRCd -- Internet Relay Chat Daemon
+ *
+ *   Copyright (C) 2013 Peter Powell <petpow@saberuk.com>
+ *
+ * This file is part of InspIRCd.  InspIRCd is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* $ModAuthor: Peter "SaberUK" Powell */
+/* $ModDesc: Allows forcing idents on users based on their connect class. */
+/* $ModDepends: core 2.0-2.1 */
+/* $ModConfig: <connect forceident="example"> */
+
+#include "inspircd.h"
+
+class ModuleForceIdent : public Module
+{
+ public:
+	void init()
+	{
+		ServerInstance->Modules->Attach(I_OnCheckReady, this);
+	}
+
+	void Prioritize()
+	{
+		ServerInstance->Modules->SetPriority(this, I_OnCheckReady, PRIORITY_LAST);
+	}
+
+	ModResult OnCheckReady(LocalUser* user)
+	{
+		ConfigTag* tag = user->MyClass->config;
+		user->ident = tag->getString("forceident", user->ident);
+		user->InvalidateCache();
+		return MOD_RES_PASSTHRU;
+	}
+
+	Version GetVersion()
+	{
+		return Version("Allows forcing idents on users based on their connect class.");
+	}
+};
+
+MODULE_INIT(ModuleForceIdent)
