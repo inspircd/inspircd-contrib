ec63edaa12a5c02dad1dc7b7355e113056d37926
Attila Molnar <attilamolnar@hush.com>
2016-02-13 16:37:50 +0100

    Add m_rehashsslsignal

---
+++ 2.0/m_rehashsslsignal.cpp
@@ -0,0 +1,65 @@
+/*
+ * InspIRCd -- Internet Relay Chat Daemon
+ *
+ *   Copyright (C) 2016 Attila Molnar <attilamolnar@hush.com>
+ *
+ * This file is part of InspIRCd.  InspIRCd is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* $ModAuthor: Attila Molnar */
+/* $ModAuthorMail: attilamolnar@hush.com */
+/* $ModDesc: Reload SSL credentials on SIGUSR1 */
+/* $ModDepends: core 2.0 */
+
+#include "inspircd.h"
+
+static volatile sig_atomic_t signaled;
+
+class ModuleRehashSSLSignal : public Module
+{
+	static void SignalHandler(int sig)
+	{
+		signaled = 1;
+	}
+
+ public:
+	~ModuleRehashSSLSignal()
+	{
+		signal(SIGUSR1, SIG_IGN);
+	}
+
+	void init()
+	{
+		ServerInstance->Modules->Attach(I_OnBackgroundTimer, this);
+		signal(SIGUSR1, SignalHandler);
+	}
+
+	void OnBackgroundTimer(time_t currtime)
+	{
+		if (!signaled)
+			return;
+
+		ServerInstance->Logs->Log("m_rehashsslsignal", DEFAULT, "Got SIGUSR1, reloading SSL credentials");
+		const std::string str = "ssl";
+		FOREACH_MOD(I_OnModuleRehash, OnModuleRehash(NULL, str));
+		signaled = 0;
+	}
+
+	Version GetVersion()
+	{
+		return Version("Reload SSL credentials on SIGUSR1");
+	}
+};
+
+MODULE_INIT(ModuleRehashSSLSignal)
