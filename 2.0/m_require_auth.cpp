0722e4da0aaa62142e544edd1d3b41e34361fa07
MuffinMedic <muffinmedic@snoonet.org>
2016-07-09 19:57:01 -0400

    Change [user@host] to <ident@host>

--- 2.0/m_require_auth.cpp
+++ 2.0/m_require_auth.cpp
@@ -144,7 +144,7 @@ class CommandGALine: public Command
 		: Command(c, linetype+"LINE", 1, 3)
 	{
 		flags_needed = 'o';
-		this->syntax = "[user@host] [<duration> :<reason>]";
+		this->syntax = "<ident@host> [<duration> :<reason>]";
 		this->linename = linetype;
 		statschar = stats;
 	}
