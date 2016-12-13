5209e3c6043d096a3974b3318d82b036cb2bfdda
linuxdemon1 <walter@walterbarnes.net>
2016-12-12 23:23:19 -0600

    Cleaned up the consonant/vowel switches

--- 2.0/m_antirandom.cpp
+++ 2.0/m_antirandom.cpp
@@ -566,55 +566,28 @@ class ModuleAntiRandom : public Module
 			}
 
 			/* Check consonants */
-            // TODO this could probably be rewritten to use strchr() and a defined string of consonants
-			switch (*s)
-			{
-				case 'b':
-				case 'c':
-				case 'd':
-				case 'f':
-				case 'g':
-				case 'h':
-				case 'j':
-				case 'k':
-				case 'l':
-				case 'm':
-				case 'n':
-				case 'p':
-				case 'q':
-				case 'r':
-				case 's':
-				case 't':
-				case 'v':
-				case 'w':
-				case 'x':
-				case 'z':
-					consonants++;
-					break;
-				default:
-					if (consonants > highest_consonants)
-						highest_consonants = consonants;
-					consonants = 0;
-					break;
-			}
+            if (strchr("bcdfghjklmnpqrstvwxz", *s))
+            {
+                consonants++;
+            }
+            else
+            {
+                if (consonants > highest_consonants)
+                    highest_consonants = consonants;
+                consonants = 0;
+            }
 
 			/* Check vowels */
-            // TODO this could probably be rewritten to use strchr() and a defined string of vowels
-			switch (*s)
-			{
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
-					vowels++;
-					break;
-				default:
-					if (vowels > highest_vowels)
-						highest_vowels = vowels;
-					vowels = 0;
-					break;
-			}
+            if (strchr("aeiou", *s))
+            {
+                vowels++;
+            }
+            else
+            {
+                if (vowels > highest_vowels)
+                    highest_vowels = vowels;
+                vowels = 0;
+            }
 		}
 
 		/* Now set up for our checks. */
