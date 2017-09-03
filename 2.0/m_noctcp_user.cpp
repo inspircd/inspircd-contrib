554758a4bb29a4b5c8a1dabdb564be3e01bb4871
Peter Powell <petpow@saberuk.com>
2017-09-03 11:25:00 +0100

    Fix a bunch of issues in m_noctcp_user.
    - Fix a compiler warning when building [-Wmisleading-indentation].
    - Change the ModDesc to describe what the module does.
    - Use the non-deprecated configuration reading system.
    - Register the mode with AddService rather than AddMode.
    - Cache the target after casting it to a User.

--- 2.0/m_noctcp_user.cpp
+++ 2.0/m_noctcp_user.cpp
@@ -21,7 +21,7 @@
 
 #include "inspircd.h"
 
-/* $ModDesc: Provides support for unreal-style user mode +T */
+/* $ModDesc: Provides user mode +T to block CTCPs */
 /* $ModDepends: core 2.0 */
 
 class NoCTCPuser : public SimpleUserModeHandler
@@ -40,43 +40,43 @@ public:
 	{
 		OnRehash(NULL);
 
-		if (!ServerInstance->Modes->AddMode(&ncu))
-			throw ModuleException("Could not add new modes!");
+		ServerInstance->Modules->AddService(ncu);
 		Implementation eventlist[] = { I_OnRehash, I_OnUserPreMessage };
-		ServerInstance->Modules->Attach(eventlist, this, 2);
+		ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
 	}
 
 	virtual Version GetVersion()
 	{
-		return Version("m_noctcpuser.cpp 2 2010-10-17 SnoFox", VF_OPTCOMMON);
+		return Version("Provides user mode +T to block CTCPs");
 	}
 
 	virtual ModResult OnUserPreMessage(User* user,void* dest,int target_type, std::string &text, char status, CUList &exempt_list)
 	{
 		if ((target_type == TYPE_USER) && (IS_LOCAL(user)))
 		{
-			if (!((User*)dest)->IsModeSet('T'))
+			User* target = static_cast<User*>(dest);
+			if (!target->IsModeSet('T'))
 				return MOD_RES_PASSTHRU;
 
 			if (operoverride && IS_OPER(user))
 				return MOD_RES_PASSTHRU;
 
-				if ((text.length()) && (text[0] == '\1'))
+			if ((text.length()) && (text[0] == '\1'))
+			{
+				if (strncmp(text.c_str(),"\1ACTION ", 8))
 				{
-					if (strncmp(text.c_str(),"\1ACTION ",8))
-					{
-						user->WriteNumeric(ERR_NOCTCPALLOWED, "%s %s :User does not accept CTCPs",user->nick.c_str(), ((User*)dest)->nick.c_str());
-						return MOD_RES_DENY;
-					}
+					user->WriteNumeric(ERR_NOCTCPALLOWED, "%s %s :User does not accept CTCPs",user->nick.c_str(), target->nick.c_str());
+					return MOD_RES_DENY;
 				}
 			}
+		}
 		return MOD_RES_PASSTHRU;
 	}
 
 	virtual void OnRehash(User* user)
 	{
-		ConfigReader Conf;
-		operoverride = Conf.ReadFlag("noctcpuser","operoverride", "0", 0);
+		ConfigTag* tag = ServerInstance->Config->ConfValue("noctcpuser");
+		operoverride = tag->getBool("operoverride");
 	}
 };
 
