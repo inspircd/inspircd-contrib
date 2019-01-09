4c6987082bc4b960b3fa7cb6fb8f3aa8f561bc26
Matt Schatz <genius3000@g3k.solutions>
2019-01-08 20:27:41 -0700

    Improve text consistency.
    - Use "-line" instead of "-Line".
    - Match wording in the addition or removal
    messages with core X-line messages.
    
    Spotted by @Robby-.

--- 2.0/m_xlinetools.cpp
+++ 2.0/m_xlinetools.cpp
@@ -18,13 +18,13 @@
 
 /* $ModAuthor: genius3000 */
 /* $ModAuthorMail: genius3000@g3k.solutions */
-/* $ModDesc: X-Line management with XCOUNT, XREMOVE, XSEARCH, and XCOPY */
+/* $ModDesc: X-line management with XCOUNT, XREMOVE, XSEARCH, and XCOPY */
 /* $ModDepends: core 2.0 */
 
 /* XCOUNT, XREMOVE, and XSEARCH are the same except for the end action:
- * XCOUNT will just return a count of matching X-Lines.
- * XREMOVE will remove the matching X-Lines and return that count.
- * XSEARCH will list the matching X-Lines and that count.
+ * XCOUNT will just return a count of matching X-lines.
+ * XREMOVE will remove the matching X-lines and return that count.
+ * XSEARCH will list the matching X-lines and that count.
  * Syntax is argument and value style "-arg=val" and is the same for each.
  * All arguments are optional:
  * -type=<xline type or *>
@@ -35,8 +35,8 @@
  * -duration=<time string> (actual set duration) Exact match, prefix '+' for longer than, or '-' for shorter than
  * -expires=<time string> (time ahead) Prefix with '+' for more than
  *
- * XCOPY copies a specified X-Line (by type and mask) to a
- * new X-Line of same type with a new mask.
+ * XCOPY copies a specified X-line (by type and mask) to a
+ * new X-line of same type with a new mask.
  * Original reason and expiry time are copied but can be
  * overridden with the 'duration' or 'reason' arguments.
  *
@@ -50,28 +50,28 @@
  * line after 'FILTER OJOIN'.
  * Find: '<helpop key="clones" ...'
  * Place just above that line:
-<helpop key="xcount" value="/XCOUNT -type=<xline type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
+<helpop key="xcount" value="/XCOUNT -type=<X-line type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
 
-Returns a count of matching X-Lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
+Returns a count of matching X-lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
 is a nick, server, or <Config>. 'Prefix your value for these 3 arguments with a '!' to negate the match. 't' is a
 time-string (seconds or 1y2w3d4h5m6s) and can be prefixed with '+' or '-' to adjust matching. All arguments are optional.">
 
-<helpop key="xremove" value="/XREMOVE -type=<xline type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
+<helpop key="xremove" value="/XREMOVE -type=<X-line type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
 
-Removes matching X-Lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
+Removes matching X-lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
 is a nick, server, or <Config>. 'Prefix your value for these 3 arguments with a '!' to negate the match. 't' is a
 time-string (seconds or 1y2w3d4h5m6s) and can be prefixed with '+' or '-' to adjust matching. All arguments are optional.
 Use /XSEARCH to test before removing.">
 
-<helpop key="xsearch" value="/XSEARCH -type=<xline type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
+<helpop key="xsearch" value="/XSEARCH -type=<X-line type|*> -mask=[!]<m> -reason=[!]<r> -source=[!]<s> -set=[-]<t> -duration=[-+]<t> -expires=[+]<t>
 
-Lists matching X-Lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
+Lists matching X-lines of the specified type (or all types). Mask(m) supports CIDR, reason(r) can include spaces, source(s)
 is a nick, server, or <Config>. Prefix your value for these 3 arguments with a '!' to negate the match. 't' is a
 time-string (seconds or 1y2w3d4h5m6s) and can be prefixed with '+' or '-' to adjust matching. All arguments are optional.">
 
-<helpop key="xcopy" value="/XCOPY <xline type> <old mask> <new mask> [-duration=<> -reason=<>]
+<helpop key="xcopy" value="/XCOPY <X-line type> <old mask> <new mask> [-duration=<> -reason=<>]
 
-Copies the specified X-Line (if found) to a new X-Line. The original reason and expiry time
+Copies the specified X-line (if found) to a new X-line. The original reason and expiry time
 are copied unless overridden with '-duration=' or '-reason='">
 
 
@@ -227,10 +227,10 @@ namespace
 	{
 		std::string typestr;
 
-		// Core/Vendor *-Lines are single character, some -extras are two
-		// Other X-Lines look better without -Lines
+		// Core/Vendor *-lines are single character, some -extras are two
+		// Other X-lines look better without -lines
 		if (type.length() <= 2)
-			typestr = type + "-Line";
+			typestr = type + "-line";
 		else
 			typestr = type;
 
@@ -401,7 +401,7 @@ class CommandXBase : public SplitCommand
 		if (args.type == "*")
 		{
 			if (!count)
-				user->WriteServ("NOTICE %s :%s matches from all X-Line types (%s)", user->nick.c_str(), action.c_str(), criteria.c_str());
+				user->WriteServ("NOTICE %s :%s matches from all X-line types (%s)", user->nick.c_str(), action.c_str(), criteria.c_str());
 
 			std::vector<std::string> xlinetypes = ServerInstance->XLines->GetAllTypes();
 			for (std::vector<std::string>::const_iterator x = xlinetypes.begin(); x != xlinetypes.end(); ++x)
@@ -420,19 +420,19 @@ class CommandXBase : public SplitCommand
 			}
 
 			if (count)
-				user->WriteServ("NOTICE %s :%u of %u X-Lines matched (%s)", user->nick.c_str(), matched, total, criteria.c_str());
+				user->WriteServ("NOTICE %s :%u of %u X-lines matched (%s)", user->nick.c_str(), matched, total, criteria.c_str());
 			else
-				user->WriteServ("NOTICE %s :End of list, %u/%u X-Lines %s", user->nick.c_str(), matched, total, (remove ? "removed" : "matched"));
+				user->WriteServ("NOTICE %s :End of list, %u/%u X-lines %s", user->nick.c_str(), matched, total, (remove ? "removed" : "matched"));
 		}
 		else
 		{
 			std::string linetype = args.type;
 			std::transform(linetype.begin(), linetype.end(), linetype.begin(), ::toupper);
 
-			// Check for the command permission to remove X-Lines of this type
+			// Check for the command permission to remove X-lines of this type
 			if (remove && !HasPermission(user, linetype))
 			{
-				user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission Denied - Oper type '%s' does not have access to remove X-Lines of type '%s'",
+				user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission Denied - Oper type '%s' does not have access to remove X-lines of type '%s'",
 						   user->nick.c_str(), user->oper->NameStr(), linetype.c_str());
 				return false;
 			}
@@ -440,24 +440,24 @@ class CommandXBase : public SplitCommand
 			XLineLookup* xlines = ServerInstance->XLines->GetAll(linetype);
 			if (!xlines)
 			{
-				user->WriteServ("NOTICE %s :Invalid X-Line type '%s' (or not yet used X-Line)", user->nick.c_str(), linetype.c_str());
+				user->WriteServ("NOTICE %s :Invalid X-line type '%s' (or not yet used X-line)", user->nick.c_str(), linetype.c_str());
 				return false;
 			}
 			if (xlines->empty())
 			{
-				user->WriteServ("NOTICE %s :No X-Lines of type '%s' exist", user->nick.c_str(), linetype.c_str());
+				user->WriteServ("NOTICE %s :No X-lines of type '%s' exist", user->nick.c_str(), linetype.c_str());
 				return false;
 			}
 
 			if (!count)
-				user->WriteServ("NOTICE %s :%s matches of X-Line type '%s' (%s)", user->nick.c_str(), action.c_str(), linetype.c_str(), criteria.c_str());
+				user->WriteServ("NOTICE %s :%s matches of X-line type '%s' (%s)", user->nick.c_str(), action.c_str(), linetype.c_str(), criteria.c_str());
 
 			ProcessLines(user, args, linetype, xlines, matched, total, count, remove);
 
 			if (count)
-				user->WriteServ("NOTICE %s :%u of %u X-Lines of type '%s' matched (%s)", user->nick.c_str(), matched, total, linetype.c_str(), criteria.c_str());
+				user->WriteServ("NOTICE %s :%u of %u X-lines of type '%s' matched (%s)", user->nick.c_str(), matched, total, linetype.c_str(), criteria.c_str());
 			else
-				user->WriteServ("NOTICE %s :End of list, %u/%u X-Lines of type '%s' %s", user->nick.c_str(), matched, total, linetype.c_str(), (remove ? "removed" : "matched"));
+				user->WriteServ("NOTICE %s :End of list, %u/%u X-lines of type '%s' %s", user->nick.c_str(), matched, total, linetype.c_str(), (remove ? "removed" : "matched"));
 		}
 
 		return true;
@@ -520,14 +520,14 @@ class CommandXCopy : public SplitCommand
 			}
 		}
 
-		// Lookup the X-Line matching what we were given
+		// Lookup the X-line matching what we were given
 		std::string linetype = xtype;
 		std::transform(linetype.begin(), linetype.end(), linetype.begin(), ::toupper);
 
-		// Check for the command permission to copy this type of X-Line
+		// Check for the command permission to copy this type of X-line
 		if (!HasPermission(user, linetype))
 		{
-			user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission Denied - Oper type '%s' does not have access to copy an X-Line of type '%s'",
+			user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission Denied - Oper type '%s' does not have access to copy an X-line of type '%s'",
 					   user->nick.c_str(), user->oper->NameStr(), linetype.c_str());
 			return CMD_FAILURE;
 		}
@@ -535,7 +535,7 @@ class CommandXCopy : public SplitCommand
 		XLineLookup* xlines = ServerInstance->XLines->GetAll(linetype);
 		if (!xlines)
 		{
-			user->WriteServ("NOTICE %s :Invalid X-Line type '%s' (or not yet used X-Line)", user->nick.c_str(), linetype.c_str());
+			user->WriteServ("NOTICE %s :Invalid X-line type '%s' (or not yet used X-line)", user->nick.c_str(), linetype.c_str());
 			return CMD_FAILURE;
 		}
 
@@ -565,7 +565,7 @@ class CommandXCopy : public SplitCommand
 			return CMD_FAILURE;
 		}
 
-		// Get the needed X-Line Factory and create the new X-Line
+		// Get the needed X-line Factory and create the new X-line
 		// Snagged this method from m_xline_db
 		XLineFactory* xlf = ServerInstance->XLines->GetFactory(linetype);
 		if (!xlf)
@@ -581,11 +581,11 @@ class CommandXCopy : public SplitCommand
 			duration = (oldxline->duration == 0 ? 0 : (oldxline->set_time + oldxline->duration - ServerInstance->Time()));
 
 		const std::string& reason = (!args.reason.empty() ? args.reason : oldxline->reason);
-		const std::string expires = (duration == 0 ? "" : (", to expire on " + ServerInstance->TimeString(ServerInstance->Time() + duration)));
+		const std::string expires = (duration == 0 ? "" : (", expires on " + ServerInstance->TimeString(ServerInstance->Time() + duration)));
 
 		XLine* newxline = xlf->Generate(ServerInstance->Time(), duration, user->nick, reason, newmask);
 		if (ServerInstance->XLines->AddLine(newxline, user))
-			ServerInstance->SNO->WriteToSnoMask('x', "%s added %s %s on %s%s: %s", user->nick.c_str(),
+			ServerInstance->SNO->WriteToSnoMask('x', "%s added %s %s for %s%s: %s", user->nick.c_str(),
 				(duration == 0 ? "permanent" : "timed"), BuildTypeStr(linetype).c_str(),
 				newmask.c_str(), expires.c_str(), reason.c_str());
 		else
@@ -623,7 +623,7 @@ class ModuleXLineTools : public Module
 
 	virtual Version GetVersion()
 	{
-		return Version("X-Line management tools", VF_OPTCOMMON);
+		return Version("X-line management tools", VF_OPTCOMMON);
 	}
 };
 
