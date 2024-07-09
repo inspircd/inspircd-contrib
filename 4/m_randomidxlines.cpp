686662f3ec4a1ac16f494cd8ed5203157943e5da
reverse <129334336+revrsedev@users.noreply.github.com>
2024-07-09 21:21:27 +0200

    Update m_randomidxlines.cpp
    changes after https://github.com/inspircd/inspircd-contrib/pull/277

--- 4/m_randomidxlines.cpp
+++ 4/m_randomidxlines.cpp
@@ -10,7 +10,7 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,46 +28,62 @@ class ModuleRandomIDxLines : public Module
 private:
     std::string GenerateRandomID()
     {
-        // Use static variables to avoid reinitializing random number generator each time
         static std::random_device rd;
         static std::mt19937 gen(rd());
-        static std::uniform_int_distribution<uint64_t> dis(100000000000000000, 999999999999999999);
-        uint64_t random_number = dis(gen);
-        return std::to_string(random_number);
+        static std::uniform_int_distribution<uint64_t> dis(1000000000, 9999999999);
+        return ConvToStr(dis(gen));
     }
 
     void AppendRandomID(std::string& message)
     {
-        // Generate the random ID to append
         std::string random_id = " - ID: " + GenerateRandomID();
-        // Ensure the message length does not exceed the limit after appending
         size_t max_reason_length = 510 - random_id.length();  // 510 to account for possible CR LF at the end
         if (message.length() > max_reason_length)
         {
             message = message.substr(0, max_reason_length);
         }
-        // Append the random ID
         message += random_id;
+    }
+
+    bool IsValidHostMask(const std::string& mask)
+    {
+        return mask.find('@') != std::string::npos || (mask.length() > 0 && mask[0] == '@');
+    }
+
+    bool IsValidDuration(const std::string& duration)
+    {
+        for (char c : duration)
+        {
+            if (!isdigit(c) && c != 's' && c != 'm' && c != 'h' && c != 'd' && c != 'w')
+                return false;
+        }
+        return true;
+    }
+
+    bool XLineExists(const std::string& command, const std::string& target)
+    {
+        if (command == "ZLINE")
+            return ServerInstance->XLines->MatchesLine("Z", target) != nullptr;
+        else if (command == "GLINE")
+            return ServerInstance->XLines->MatchesLine("G", target) != nullptr;
+        else if (command == "KLINE")
+            return ServerInstance->XLines->MatchesLine("K", target) != nullptr;
+        return false;
     }
 
     ModResult HandleLineCommand(const std::string& command, User* source, CommandBase::Params& parameters)
     {
-        if (parameters.size() > 1)
+        // Only add random ID if there are enough parameters to set a line (target, duration, reason)
+        if (parameters.size() > 2 && IsValidHostMask(parameters[0]) && IsValidDuration(parameters[1]))
         {
-            // Append random ID to the existing reason parameter
-            AppendRandomID(parameters[1]);
+            if (!XLineExists(command, parameters[0]))
+            {
+                // Append random ID to the existing reason parameter
+                AppendRandomID(parameters.back());
+                std::string log_message = INSP_FORMAT("{} {} {}: {}", source->nick, command, parameters[0], parameters.back());
+                ServerInstance->SNO.WriteToSnoMask('a', log_message);
+            }
         }
-        else
-        {
-            // Create a new reason with the random ID if it doesn't exist
-            std::string id_message = "- ID: " + GenerateRandomID();
-            AppendRandomID(id_message);
-            parameters.push_back(id_message);
-        }
-
-        // Log the command with the appended random ID
-        std::string log_message = fmt::format("{} {} {}: {}", source->nick, command, parameters[0], parameters[1]);
-        ServerInstance->SNO.WriteToSnoMask('a', log_message);
 
         return MOD_RES_PASSTHRU;
     }
@@ -83,8 +99,8 @@ public:
         if (!validated)
             return MOD_RES_PASSTHRU;
 
-        // Handle specific commands to append the random ID
-        if (command == "ZLINE" || command == "GLINE" || command == "KLINE" || command == "KILL")
+        // Handle commands
+        if ((command == "ZLINE" || command == "GLINE" || command == "KLINE" || command == "KILL"))
         {
             return HandleLineCommand(command, user, parameters);
         }
@@ -94,3 +110,4 @@ public:
 };
 
 MODULE_INIT(ModuleRandomIDxLines)
+
