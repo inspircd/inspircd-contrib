203de04f52437240e156977e8cc6dfb0f72af79a
Sadie Powell <sadie@witchery.services>
2024-09-05 22:14:08 +0100

    Add missing module changes to upgradecheck.

--- 3/m_upgradecheck.cpp
+++ 3/m_upgradecheck.cpp
@@ -129,11 +129,13 @@ class ModuleUpgradeCheck CXX11_FINAL
 		CheckModule("hostchange", "moved to inspircd-contrib");
 		CheckModule("lockserv", "moved to inspircd-contrib");
 		CheckModule("modenotice", "moved to inspircd-contrib");
+		CheckModule("regex_pcre", "moved to inspircd-contrib");
 		CheckModule("regex_tre", "moved to inspircd-contrib");
+		CheckModule("ssl_mbedtls", "moved to inspircd-contrib");
 		CheckModule("userip", "moved to inspircd-contrib");
 		CheckNoValue("class", "snomasks", "no longer defaults to *");
-		CheckTag("power", "replaced with oper command privs");
 		CheckTag("hostchange", "replaced with the cloak_user and cloak_static <cloak> methods");
+		CheckTag("power", "replaced with oper command privs");
 		CheckValue("bind", "sslprofile", "gnutls", "ssl config moved from <gnutls> to <sslprofile>");
 		CheckValue("bind", "sslprofile", "mbedtls", "ssl config moved from <mbedtls> to <sslprofile>");
 		CheckValue("bind", "sslprofile", "openssl", "ssl config moved from <openssl> to <sslprofile>");
